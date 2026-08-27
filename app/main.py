47bc44ae55586d4a86932afdd1645847ff0d8679
gelane-dev <leonardo.gelane.santos@gmail.com>
2026-08-27 18:45:21 -0300

    feat: adiciona as rotas cadastro e login

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from .database import get_db
+from schemas.usuario import UsuarioCriar, UsuarioResposta, UsuarioLogin
+from .models import User
+from core.seguranca import criar_hash, verificar_senha
 
 app = FastAPI(
     title="Academia API",
@@ -8,6 +12,43 @@ app = FastAPI(
     version="1.0.0"
 )
 
-@app.get("/login")
-def root(db: Session = Depends(get_db)):
-    return {"message": "API funcionando"}
+@app.post("/login")
+def logar(dados: UsuarioLogin, db: Session = Depends(get_db)):
+
+    usuario = db.scalar(
+select(User).where(User.email == dados.email))
+    
+    if not usuario:
+        raise HTTPException(
+            status_code=401,
+            detail="E-mail ou senha inválidos"
+        )
+    if not verificar_senha(dados.senha,usuario.senha):
+        raise HTTPException(
+            status_code=401,
+            detail="E-mail ou senha inválidos"
+        )
+
+@app.post("/cadastro", response_model=UsuarioResposta)
+def criar_usuario(dados: UsuarioCriar, db: Session = Depends(get_db)):
+
+    usuario = User(
+        nome=dados.nome,
+        email=dados.email,
+        senha=criar_hash(dados.senha)
+    )
+
+    usuario_existetnte = db.scalar(
+select(User).where(User.email == dados.email))
+    
+    if usuario_existetnte:
+        raise HTTPException(
+            status_code=400,
+            detail="E-mail já cadastrado"
+        )
+
+    db.add(usuario)
+    db.commit()
+    db.refresh(usuario)
+
+    return usuario
