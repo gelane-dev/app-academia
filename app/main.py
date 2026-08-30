7b23567649b105f6d63bbf47c1a285079606aa2f
gelane-dev <leonardo.gelane.santos@gmail.com>
2026-08-30 17:58:46 -0300

    feat: implementar autenticacao e rotas de usuario

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from .database import get_db
-from schemas.usuario import UsuarioCriar, UsuarioResposta, UsuarioLogin
+from .schemas.usuario import UsuarioCriar, UsuarioResposta, UsuarioLogin
 from .models import User
-from core.seguranca import criar_hash, verificar_senha
+from .core.seguranca import criar_hash, verificar_senha, criar_token, verificar_token
 
 app = FastAPI(
     title="Academia API",
@@ -29,19 +29,26 @@ select(User).where(User.email == dados.email))
             detail="E-mail ou senha inválidos"
         )
 
+    token = criar_token(usuario.id)
+
+    return {
+    "access_token": token,
+    "token_type": "bearer"
+}   
+
+
 @app.post("/cadastro", response_model=UsuarioResposta)
 def criar_usuario(dados: UsuarioCriar, db: Session = Depends(get_db)):
 
     usuario = User(
-        nome=dados.nome,
         email=dados.email,
         senha=criar_hash(dados.senha)
     )
 
-    usuario_existetnte = db.scalar(
+    usuario_existente = db.scalar(
 select(User).where(User.email == dados.email))
     
-    if usuario_existetnte:
+    if usuario_existetente:
         raise HTTPException(
             status_code=400,
             detail="E-mail já cadastrado"
@@ -51,4 +58,20 @@ select(User).where(User.email == dados.email))
     db.commit()
     db.refresh(usuario)
 
+    return usuario
+
+@app.get("/perfil", response_model=UsuarioResposta)
+def meu_perfil(db: Session = Depends(get_db), verificar: dict = Depends(verificar_token)):
+
+    id_usuario = int(verificar["sub"])
+    
+    usuario = db.scalar(
+    select(User).where(User.id == id_usuario))
+
+    if not usuario:
+        raise HTTPException(
+            status_code=401,
+            detail="Usuario não encontado"
+        )
+    
     return usuario
