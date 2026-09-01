dfe5463ffd38d3a62acd7e42cac4cb436e1686e3
gelane-dev <leonardo.gelane.santos@gmail.com>
2026-09-01 00:54:01 -0300

    feat: Adiciona EmailAtualizar, SenhaAtualizar

--- app/schemas/usuario.py
+++ app/schemas/usuario.py
@@ -6,7 +6,10 @@ class UsuarioBase(BaseModel):
 
 class UsuarioCriar(UsuarioBase):
     senha: str
-    
+    nome: str
+    telefone: str
+    data_nascimento: date
+
 class UsuarioResposta(UsuarioBase):
     id: int
 
@@ -19,6 +22,24 @@ class AlunoAtualizar(BaseModel):
     telefone: str
     data_nascimento: date
 
+class AlunoResposta(BaseModel):
+    id: int
+    nome: str
+    telefone: str
+    data_nascimento: date
+
 class ProfessorAtualizar(BaseModel):
     nome: str
     telefone: str
+
+class ProfessorResposta(BaseModel):
+    id: int
+    nome: str
+    telefone: str
+
+class EmailAtualizar(BaseModel):
+    email: EmailStr
+
+class SenhaAtualizar(BaseModel):
+    senha_atual: str
+    nova_senha: str
