fc92216160048a6fa2e26ba04f47740019fb6448
gelane-dev <leonardo.gelane.santos@gmail.com>
2026-09-03 14:14:25 -0300

    fix(routers): corrige erro em import que causava bug

--- app/routers/main.py
+++ app/routers/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import auth, usuarios, contas, admin, exercicios
+from app.routers import auth, usuarios, admin, exercicios
 
 app = FastAPI(
     title="Academia API",
@@ -9,6 +9,5 @@ app = FastAPI(
 
 app.include_router(auth.router)
 app.include_router(usuarios.router)
-app.include_router(contas.router)
 app.include_router(admin.router)
 app.include_router(exercicios.router)
