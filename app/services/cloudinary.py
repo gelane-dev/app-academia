f1611ee3df58f25d659c8e51e0b419ece60d7ab6
gelane-dev <leonardo.gelane.santos@gmail.com>
2026-09-03 14:16:49 -0300

    feat(services): adiciona funções de upload e delete de imagens e videos

--- app/services/cloudinary.py
+++ app/services/cloudinary.py
@@ -1,4 +1,5 @@
 import cloudinary
+import cloudinary.uploader
 from app.core.config import (CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET)
 
 cloudinary.config(
@@ -6,3 +7,33 @@ cloudinary.config(
     api_key=CLOUDINARY_API_KEY,
     api_secret=CLOUDINARY_API_SECRET
 )
+
+def upload_imagem(arquivo):
+    
+    resultado = cloudinary.uploader.upload(arquivo.file)
+
+    url = resultado["secure_url"]
+
+    return url
+
+def upload_video(arquivo):
+    
+    resultado = cloudinary.uploader.upload(arquivo.file, resource_type="video")
+
+    url = resultado["secure_url"]
+    id_video = resultado["public_id"]
+    
+    return url, id_video
+
+def deletar_imagem(public_id):
+
+    resultado = cloudinary.uploader.destroy(public_id, resource_type="image")
+
+    return resultado
+
+def deletar_video(public_id):
+
+    resultado = cloudinary.uploader.destroy(public_id, resource_type="video")
+
+    return resultado
+
