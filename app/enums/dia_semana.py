94cb7d5c675cf17a8d9d08ca4472dd0cddb7304d
gelane-dev <leonardo.gelane.santos@gmail.com>
2026-09-08 17:26:17 -0300

    feat: adiciona enum para dias da semana

---
+++ app/enums/dia_semana.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class DiaSemana(str, Enum):
+    segunda = "segunda"
+    terca = "terça"
+    quarta = "quarta"
+    quinta = "quinta"
+    sexta = "sexta"
+    sabado = "sábado"
+    domingo = "domingo"
